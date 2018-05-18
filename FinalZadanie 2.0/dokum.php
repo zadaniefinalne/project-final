fea8ce63800264dd7b2483489d57b999b763076d
frantisek.ff <frantisek.ff@gmail.com>
2018-05-18 19:20:01 +0200

    doplnena dokumentacia

--- FinalZadanie 2.0/dokum.php
+++ FinalZadanie 2.0/dokum.php
@@ -1,7 +1,16 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
 
 
 <nav class="navbar navbar-expand-sm bg-dark">
@@ -17,4 +26,47 @@
         </li>
     </ul>
 </nav>
+
+<div class="row">
+
+    <div class="col-md-4 offset-4 " style="word-wrap: break-word;">
+        <br> <br>
+        Na testovanie projektu sme vytvorili dva učty. Jeden administrátorsky a jedného užívateľa.
+        <br>
+        <strong>Administrátorské meno je admin@admin.admin a heslo 123456.</strong>
+        <br>
+        <strong>Užívateľsky účet meno: frantisek.ff.ff@gmail.com a heslo frantisek</strong>
+        <br>
+        Projekt sa nachádza na adrese: https://147.175.98.137/final
+        <br>Prihlásiť sa môžeme v sekcii login a zaregistrovať s mailom v sekcii sign up.
+        <br> <br>
+        <h2>Bežný užívateľ:</h2>
+        Po prihlásení do aplikácie si môže vytvoriť trasu – začiatočný a konečný bod.
+        Následne si ju v sekcii traceList môže zobraziť a zadať aký urobil pokrok na tejto trase. V sekcii TraceList sa
+        zobrazujú aj všetky verejné trasy + štafety.
+        Všetky svoje tréningy si môže pozrieť v sekcii Activities.
+        Aktuálne novinky nájde v sekcii Newsletter, kde sa môže na odber prihlásiť/odhlásiť.
+        <br> <br>
+        <h2>Administrátor:</h2>
+        Má možnosť vytvoriť viacerých používateľov nahratím jedného súboru. Ďalej v sekcii Teams vidí všetky tímy. V
+        sekcii Add Team môže vytvárať nové tímy.
+        Sekcia Users zobrazuje všetkých užívateľov. Po rozkríknutí užívateľa sa zobrazí tabuľka so všetkými aktivitami
+        užívateľa.
+
+        <br> <br>
+        <h2>Použité API</h2>
+        Google maps JavaScript API
+        JsPDF
+        <br> <br>
+        <h2>Rozdelenie úloh</h2>
+
+        <br>Vladimír Ďurčík a Michal Firča - úlohy 1. - 5.
+        <br>Richard Szabo a Richard Friedl - úlohy 6. - 10.
+        <br>František Franko - úlohy 11. - 13.
+
+    </div>
+</div>
+</body>
+</html>
+
 
